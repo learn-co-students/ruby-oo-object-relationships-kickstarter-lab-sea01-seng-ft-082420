6d06f5a12e2521363d5e4ac1a8c973cf14257a3e
JacksonBey <jacksongrowson@gmail.com>
2020-08-27 11:54:04 -0700

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,52 @@
+require 'pry'
+class Backer
+attr_reader :name
+
+    def initialize(name)
+        @name=name
+    end
+
+    def back_project(project)
+        project=ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        backerp = ProjectBacker.all.select do |backer|
+           backer.backer == self
+        end
+        parray= []
+        backerp.each do |key|
+            parray << key.project
+        end
+        return parray
+    end
+end
+
+=begin
+Backer - ::new
+  takes a name on initialization, accessible through an attribute reader (FAILED - 1)
+
+Project - ::new
+  takes a title on initialization, accessible through an attribute reader (FAILED - 2)
+
+ProjectBacker
+  @@all
+    is a class variable set to an empty array (FAILED - 3)
+  .all
+    is a class method that returns the @@all class variable (FAILED - 4)
+  ::new
+    takes in a project and a backer on initialization, accessible through an attribute reader (FAILED - 5)
+    stores the new instance of ProjectBacker in the @@all class variable (FAILED - 6)
+
+Backer - #back_project
+  is an instance method that accepts a project as an argument and creates a ProjectBacker, associating the project with this backer (FAILED - 7)
+
+Project - #add_backer
+  is an instance method that accepts a backer as an argument and creates a ProjectBacker, associating the backer with this project (FAILED - 8)
+
+Backer - backed_projects
+  returns an array of projects associated with this Backer instance (FAILED - 9)
+
+Project - backers
+  returns an array of backers associated with this Project instance (FAILED - 10)
+=end
