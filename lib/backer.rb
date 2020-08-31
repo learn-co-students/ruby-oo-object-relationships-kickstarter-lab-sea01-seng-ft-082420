2b8d7b60d79c80b3c78f294f427e82c7ccbf03d4
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-30 20:15:12 -0700

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,19 +1,21 @@
+require 'pry'
 class Backer
-  attr_reader :name
-      def initialize(name)
-          @name=name
-      end
-      def back_project(project)
-          project=ProjectBacker.new(project, self)
-      end
-      def backed_projects
-          backerp = ProjectBacker.all.select do |backer|
-             backer.backer == self
-          end
-          parray= []
-          backerp.each do |key|
-              parray << key.project
-          end
-          return parray
-      end
+    attr_reader :name 
+
+    def initialize(name)
+        @name = name 
+    end 
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end 
+
+    def helper
+        ProjectBacker.all.select {|info| info.backer == self}
+    end 
+
+    def backed_projects
+        helper.map {|info| info.project}
+    end
+# binding.pry 
   end
--- lib/project.rb
+++ lib/project.rb
@@ -1,19 +1,20 @@
 class Project
-  attr_reader :title
-  def initialize(title)
-      @title=title
-  end
-  def add_backer(backer)
-      backer=ProjectBacker.new(self, backer)
-  end
-  def backers
-      backerp = ProjectBacker.all.select do |backer|
-         backer.project == self
-      end
-      parray= []
-      backerp.each do |key|
-          parray << key.backer
-      end
-      return parray
-  end
+    attr_reader :title 
+
+    def initialize(title)
+        @title = title 
+    end 
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end 
+
+    def helper
+        ProjectBacker.all.select {|info| info.project == self}
+    end 
+
+    def backers
+        helper.map {|info| info.backer}
+    end 
+
 end
